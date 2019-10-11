1ce6a93580d03f7bdfc701a04a968686041ab08d
jihye <zehye.01@gmail.com>
2019-10-11 13:46:28 +0900

    단순연결 리스트 Node 정리

---
+++ src/Node.java
@@ -0,0 +1,14 @@
+public class Node <E> {
+    private E item;
+    private Node<E> next;
+
+    public Node(E newItem, Node<E> node){
+        item = newItem;
+        next = node;
+    }
+
+    public E getItem() {return item;}
+    public Node<E> getNext() {return next;}
+    public void setItem(E newItem) {item=newItem;}
+    public void setNext(Node<E> newNext) {next=newNext;}
+}
